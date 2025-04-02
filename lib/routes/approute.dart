7896a422ffec99c74b74a44644127b8428626fe9
Maria Rojas <mrojas.7441@unimar.edu.ve>
2025-04-02 18:56:31 -0400

    commit productos y botones

--- lib/routes/approute.dart
+++ lib/routes/approute.dart
@@ -1,13 +1,16 @@
 import 'package:flutter/material.dart';
 import 'package:miapp_flutter/contact.dart';
+import 'package:miapp_flutter/home.dart';
 import 'package:miapp_flutter/menu.dart';
 
 
 class Approute {
-  static const initialroute = "/menu";
-  static const routecontact = "/contact";
+  static const initialroute = '/menu';
+  static const routecontact = '/contact';
+
   static Map<String, Widget Function(BuildContext)> routes = { 
         '/contact': (context) => const ContactPage(),
         '/menu': (context) => const MenuPage(),
+        '/home': (context) => const HomePage(),
         };
 }
